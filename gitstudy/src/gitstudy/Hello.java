f38a9d06b22279acc7ac24604f449a879a760418
1617552960 <69193242+1617552960@users.noreply.github.com>
2020-08-19 16:31:27 +0800

    Update Hello.java

--- gitstudy/src/gitstudy/Hello.java
+++ gitstudy/src/gitstudy/Hello.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("79648");
 	System.out.println("123456789");
 	System.out.println("1856");
-	System.out.println("1999");
+	System.out.println("--------");
+        System.out.println("9999");
 }
 }
